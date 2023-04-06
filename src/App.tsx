3fff0fd7db3d4b81d5d75e3b005f80f75085e1a8
ozzkan-burak <brk.ozzkan@gmail.com>
2023-04-06 23:18:14 +0300

    episode5

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { add } from "./features/todotSlice";
 import './App.css';
-import { useAppDispatch } from './store';
+import { useAppDispatch, useAppSelector } from './store';
 
 function App() {
+  const todos = useAppSelector(state => state.todos)
   const [title, setTitle] = useState("")
 
   const dispatch = useAppDispatch();
@@ -17,6 +18,10 @@ function App() {
     <div className="App">
       <input name="title" value={title} onChange={(e) => setTitle(e.currentTarget.value)} />
       <button onClick={onSave}>Save</button>
+      <ul>
+        {todos.map((todo)=> console.log(todo)
+        )}
+      </ul>
     </div>
   );
 }
